ca991ad1852cbadb7ec01645fff69c159d30d5f8
Brian Meneses <brian.meneses@gmail.com>
2012-03-29 20:30:44 -0700

    Finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,8 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:session][:email])
     if user && user.authenticate(params[:session][:password])
       sign_in user
-      redirect_to user
-      
+      redirect_back_or user # instead of redirect_to user, use helper to figure out where
+                            # the user attempted to access before logging in
     else
       #error out and re-render the signin form
       flash.now[:error] = 'Invalid email/password combination' # not right?
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -17,6 +17,11 @@ module SessionsHelper
     @current_user ||= user_from_remember_token
   end
   
+  def current_user?(user)
+    user == current_user
+  end
+  
+  
   #check to see if signed_in
   def signed_in?
     !current_user.nil?
@@ -27,6 +32,19 @@ module SessionsHelper
     cookies.delete(:remember_token)
   end
   
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    clear_return_to
+  end
+  
+  def store_location
+    session[:return_to] = request.fullpath
+  end
+  
+  
+  
+  
+  
   
   private
     
@@ -35,5 +53,8 @@ module SessionsHelper
       User.find_by_remember_token(remember_token) unless remember_token.nil?
     end
     
+    def clear_return_to
+      session.delete(:return_to)
+    end
   
 end
